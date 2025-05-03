2ea8152a7c1ca2b893359491bd78682ee6a01278
Yana Rudometova <rudometova.ya@gmail.com>
2025-05-03 14:27:59 +0300

    add js_form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,88 @@
-console.log('yaayay');
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('.email');
+const massage = form.querySelector('.message');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+fillTheForm();
+
+form.addEventListener('submit', handelFormSubmit);
+form.addEventListener('input', handelFormlInput);
+
+function handelFormSubmit(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData = {
+    email: '',
+    message: '',
+  };
+
+  form.reset();
+}
+
+function handelFormlInput(event) {
+  const { name, value } = event.target;
+
+  if (name === 'email' || name === 'message') {
+    formData[name] = value.trim();
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+// Функціонал функуії по прикладу з відеоуроку від ментора( більш складнвший для мене)//
+
+function fillTheForm() {
+  const saveLSData = localStorage.getItem(STORAGE_KEY);
+
+  if (!saveLSData) return;
+
+  try {
+    const dataFromLS = JSON.parse(saveLSData);
+    const formEL = new FormData(form);
+    const formFields = Array.from(formEL.keys());
+
+    formFields.forEach(field => {
+      form.elements[field].value = dataFromLS[field];
+      formData[field] = dataFromLS[field];
+    });
+  } catch (error) {
+    console.error('Error reading localStorage data', error);
+  }
+}
+
+// Функціонал функуії більш простіший для мене)//
+
+/* function fillTheForm() {
+  const savedLSData = localStorage.getItem(STORAGE_KEY);
+
+  if (!savedLSData) return; 
+
+  try {
+    const dataFromLS = JSON.parse(savedLSData);
+
+    
+    for (const field in dataFromLS) {
+      if (dataFromLS.hasOwnProperty(field)) {
+        form.elements[field].value = dataFromLS[field]; 
+        formData[field] = dataFromLS[field]; 
+      }
+    }
+
+  } catch (error) {
+    console.error("Error reading localStorage data", error);
+  }
+} */
